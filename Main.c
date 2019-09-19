54c59fa21f657a538ec3b017f2bc2d2f1b2997fd
GuardianWorld <emanuelvitorsam@gmail.com>
2019-09-18 22:05:34 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -17,7 +17,9 @@ int main()
 		printf("1) Inicializa\n");
 		printf("2) Insere\n");
 		printf("3) Imprime\n");
-		printf("4) Inserir Pos");
+		printf("4) Inserir Pos\n");
+		printf("5) Retirar\n");
+		printf("6) RetirarPos\n");
 		printf("0) Sair \n");
 		scanf("%d", &input);
 		if(input == 0)
@@ -43,6 +45,15 @@ int main()
 			scanf("%d", &pos);
 			scanf("%d", &item);
 			error = LInserePos(&lista, &item, pos);
+		}
+		else if (input == 5)
+		{
+			error = LRetira(&lista, &item);
+		}
+		else if (input == 6)
+		{
+			scanf("%d", &pos);
+			error = LRetiraPos(&lista, &item, pos);
 		}
 		
 	}
