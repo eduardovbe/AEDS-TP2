e9af31d1dbd87c4c853614a95fa49700f5334b6a
GuardianWorld <emanuelvitorsam@gmail.com>
2019-09-19 23:13:06 -0300

    Update Matriz.h

--- Matriz.h
+++ Matriz.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CABECA -1
+
 typedef double TipoChave;
 typedef struct {
 	TipoChave Chave;
@@ -15,22 +17,35 @@ typedef struct Celula {
 	struct Celula* abaixo;  /* Apontador para a casa abaixo; */
 } TCelula;
 
+typedef int matriz;
 typedef struct {
 	Apontador pPrimeiro;
 	Apontador pUltimo;
+	matriz X;
+	matriz Y;
 } TLista;
+
+typedef struct {
+	Apontador pPrimeiro;
+	matriz X;
+	matriz Y;
+} TMatriz;
 
 //Valores: -1 < Cabeça
 //struct Celula* e um ponteiro...
-//Alocar somente oque vai usar.
-void imprimeMatriz(); // Aqui vai fazer a Matriz na tela
+//Alocar somente oque vai usar. // Aqui vai fazer a Matriz na tela
 
-void LeMatriz(FILE *OFILE); //Aqui vai ler de um arquivo, e montar a Matriz.
+void LeMatriz(FILE *OFILE, TMatriz* pMatriz, TItem *item); //Aqui vai ler de um arquivo, e montar a Matriz.
+void ImprimeMatriz(TMatriz* pMatriz);
 
-void FLVazia(TLista* pLista);
+void InicializarCabeça(TMatriz* pMatriz); //Inicializou 
+void InicializarCabeçaLinha(TMatriz* pMatriz);
+void InicializarCabeçaColuna(TMatriz* pMatriz);
+void Insere(TMatriz* pMatriz, float pItem, int Linha, int Coluna);
+
 int LEhVazia(TLista* pLista);
 int LInsere(TLista* pLista, TItem *pItem);
 int LInserePos(TLista *pLista, TItem* pItem, int pos);
 int LRetira(TLista* pLista, TItem *pItem);
 int LRetiraPos(TLista* pLista, TItem* pItem, int pos);
-void LImprime(TLista* pLista);
+
