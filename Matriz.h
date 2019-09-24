9fa22730cdd245e02e48a3c015b0cee556574a66
GuardianWorld <emanuelvitorsam@gmail.com>
2019-09-24 04:27:28 -0300

    Update Matriz.h

--- Matriz.h
+++ Matriz.h
@@ -1,30 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Produto.h"
 
 #define CABECA -1
 
-typedef double TipoChave;
-typedef struct {
-	TipoChave Chave;
-	/* outros componentes */
-} TItem;
-
 typedef struct Celula* Apontador;
 typedef struct Celula {
-	TItem Item;
+	struct Celula* direita;
+	struct Celula* abaixo;
+	double valor;
 	int linha, coluna;
-	struct Celula* direita; /* Apontador para a Proxima casa; */
-	struct Celula* abaixo;  /* Apontador para a casa abaixo; */
 } TCelula;
 
-typedef int matriz;
+/*
 typedef struct {
 	Apontador pPrimeiro;
 	Apontador pUltimo;
 	matriz X;
 	matriz Y;
 } TLista;
-
+*/
+typedef int matriz;
 typedef struct {
 	Apontador pPrimeiro;
 	matriz X;
@@ -38,14 +34,16 @@ typedef struct {
 void LeMatriz(FILE *OFILE, TMatriz* pMatriz, TItem *item); //Aqui vai ler de um arquivo, e montar a Matriz.
 void ImprimeMatriz(TMatriz* pMatriz);
 
-void InicializarCabeça(TMatriz* pMatriz); //Inicializou 
-void InicializarCabeçaLinha(TMatriz* pMatriz);
-void InicializarCabeçaColuna(TMatriz* pMatriz);
-void Insere(TMatriz* pMatriz, float pItem, int Linha, int Coluna);
-
+void InicializarCabeca(TMatriz* pMatriz); //Inicializou 
+void InicializarCabecaLinha(TMatriz* pMatriz);
+void InicializarCabecaColuna(TMatriz* pMatriz);
+void Insere(TMatriz* pMatriz, double pItem, int Linha, int Coluna);
+Apontador getColuna(Apontador linhaCabeca, int coluna);
+Apontador getLinha(Apontador linhaCabeca, int linha);
+/*
 int LEhVazia(TLista* pLista);
 int LInsere(TLista* pLista, TItem *pItem);
 int LInserePos(TLista *pLista, TItem* pItem, int pos);
 int LRetira(TLista* pLista, TItem *pItem);
 int LRetiraPos(TLista* pLista, TItem* pItem, int pos);
-
+*/
