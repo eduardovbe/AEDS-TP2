a88ad20f5b969c37750b1b9b8684b85d06f6bfc0
eduardovbe <51214452+eduardovbe@users.noreply.github.com>
2019-09-25 00:19:14 -0300

    Create funções.c

---
+++ funções.c
@@ -0,0 +1,91 @@
+#include "Produto.h"
+void quantidadeComprasPorCliente(TMatriz* pMatriz){
+    int i,j,h,quantidade = 0;
+    TCelula *Aux;
+	TCelula *Linha;
+
+
+	Linha = pMatriz->pPrimeiro->abaixo;
+	Aux = Linha;
+    Aux = TMatriz->pPrimeiro->direita->abaixo;
+	for (y = 1; y <= pMatriz->Y; y++) {
+		for (x = 1; x <= pMatriz->X; x++) {
+            if((Aux->linha || Aux->coluna) != CABECA){
+                while(Aux->Item->pProx != NULL){
+                    quantidade++;
+                    Aux->Item = Aux->Item->pProx;
+                    }
+                
+                printf("\ncliente %d realizou %d compras",Aux->linha ,quantidade);
+                
+            }
+            Aux = Aux->direita;
+        }
+        Linha = Linha->abaixo;
+		Aux = Linha;    
+    }
+		
+}
+
+
+
+void quantidadeComprasPorProduto(TMatriz* pMatriz){
+    int i,j,h,quantidade = 0;
+    TCelula *Aux;
+	TCelula *Coluna;
+
+
+	Linha = pMatriz->pPrimeiro->abaixo;
+	Aux = Coluna;
+    Aux = TMatriz->pPrimeiro->direita->abaixo;
+	for (y = 1; y <= pMatriz->Y; y++) {
+		for (x = 1; x <= pMatriz->X; x++) {
+            if((Aux->linha || Aux->coluna) != CABECA){
+                while(Aux->Item->pProx != NULL){
+                    quantidade++;
+                    Aux->Item = Aux->Item->pProx;
+                    }
+                
+                printf("\nproduto %d foi comprado %d vezes",Aux->coluna ,quantidade);
+                
+            }
+            Aux = Aux->abaixo;
+        }
+        Coluna = Coluna->direita;
+		Aux = Coluna;
+    }
+		
+}
+
+
+    
+
+
+
+void ImprimeMatriz(TMatriz* pMatriz) {
+	int y, x;
+	Apontador Aux;
+	Apontador Linha;
+
+
+	Linha = pMatriz->pPrimeiro->abaixo;
+	Aux = Linha;
+
+    //printf("\n%f\n\n", pMatriz->pPrimeiro->abaixo->abaixo->abaixo->direita->valor);
+	for (y = 1; y <= pMatriz->Y; y++) {
+		printf("\n[");
+		for (x = 1; x <= pMatriz->X; x++) {
+            while(Aux->Item->pProx != NULL){
+                if (Aux->direita->linha == y && Aux->direita->coluna == x) {
+                    Aux->Item = Aux->Item->pProx;
+                    printf(" %d %s ",Aux->Item->quantidade, Aux->Item->data);
+                }
+                
+            }
+            Aux = Aux->direita;
+		}
+		Linha = Linha->abaixo;
+		Aux = Linha;
+		printf("]\n");
+	}
+}
