287ff0698293dbe8125a396bc715461e4d8bd339
GuardianWorld <emanuelvitorsam@gmail.com>
2019-09-26 01:11:25 -0300

    Update and rename Matriz.c to TAD_Matriz.c

--- Parte1/TAD_Matriz.c
+++ Parte1/TAD_Matriz.c
@@ -1,17 +1,12 @@
-#include "Matriz.h"
+#include "TAD_Matriz.h"
 
-//int debug = 0;
-//int debug;
-void LeMatriz(FILE *OFILE, TMatriz* pMatriz, TItem *item)
+void LeMatriz(FILE *OFILE, TMatriz* pMatriz, double *item)
 {
-	item->Chave = 0;
-	printf("\nMudar nome da TLista para TMatriz e retirar PUltimo, Deletar lixo de memoria!!!\n");
 	char fileName[50];
 	strcpy(fileName, "Matriz.txt");
 	OFILE = fopen(fileName, "r");
 	fscanf(OFILE, "%d, %d", &pMatriz->X, &pMatriz->Y);
-	//printf("%d x %d\n",pMatriz -> X,pMatriz -> Y);
-	InicializarCabeca(pMatriz); //Inicializando com sucesso
+	InicializarCabeca(pMatriz);
 	int L, C;
 	double I;
 	while (fscanf(OFILE, "%d, %d, %lf", &L, &C, &I) != EOF)
@@ -75,7 +70,6 @@ void ImprimeMatriz(TMatriz* pMatriz) {
 	Linha = pMatriz->pPrimeiro->abaixo;
 	Aux = Linha;
 
-    //printf("\n%f\n\n", pMatriz->pPrimeiro->abaixo->abaixo->abaixo->direita->valor);
 	for (y = 1; y <= pMatriz->Y; y++) {
 		printf("\n[");
 		for (x = 1; x <= pMatriz->X; x++) {
@@ -99,7 +93,6 @@ Apontador getColuna(Apontador linhaCabeca, int coluna){
     for(i = 0; (i < coluna) && (aux->direita->linha != CABECA); i++){
         aux = aux->direita;
     }
-    //printf("\tgetColuna Desejo = %d | Consegui = %d\n", coluna, aux->coluna);
     if(aux->coluna == coluna)
         return aux;
     else
@@ -112,7 +105,6 @@ Apontador getLinha(Apontador linhaCabeca, int linha){
     for(i = 0; i < linha; i++){
         aux = aux->abaixo;
     }
-	//printf("\tgetLinha Desejo = %d | Consegui = %d\n", linha, aux->linha);
     return aux;
 }
 
@@ -153,10 +145,8 @@ void Insere(TMatriz* pMatriz, double pItem, int Linha, int Coluna)
     Aux->linha = Linha;
     Aux->valor = pItem;
     Aux->abaixo = AndadorColuna; // Por padrão aponta para a Cabeça Coluna
-    //printf("\tAdicionado Linha %d | Coluna %d\n", Linha, Coluna);
     Apontador linhaAbaixo = AndadorLinha->abaixo; // Se ecncontrar algum elemento abaixo, aponta para ele
     for(i = Linha+1; i < (pMatriz->Y+1); i++){
-        //printf("\tTentando Linha %d", i);
         Apontador celulaAbaixo = getColuna(linhaAbaixo, Coluna);
         if(celulaAbaixo != NULL){
             Aux->abaixo = celulaAbaixo;
@@ -177,88 +167,3 @@ void Insere(TMatriz* pMatriz, double pItem, int Linha, int Coluna)
         }
     }
 }
-
-/*
-int LEhVazia(TLista* pLista)
-{
-	return (pLista->pPrimeiro == pLista->pUltimo);
-}
-
-int LInsere(TLista *pLista, TItem* pItem)
-{
-	pLista->pUltimo->direita = (Apontador)malloc(sizeof(TCelula));
-	pLista->pUltimo = pLista->pUltimo->direita;
-	pLista->pUltimo->Item = *pItem;
-	pLista->pUltimo->direita = pLista->pPrimeiro;
-	//Matriz : 4x4
-	//For x < 4;, Y < 4;
-	return 1;
-}
-
-int LInserePos(TLista *pLista, TItem* pItem, int pos)
-{
-	Apontador pAux;
-	Apontador ptr;
-	int x = 0;
-	pAux = pLista->pPrimeiro->direita; // Cabeça -> Direita
-	ptr = pLista->pPrimeiro; //Começa na Cabeça
-	while (x <= pos)
-	{
-
-		if (x == pos)
-		{
-			ptr->direita = (Apontador)malloc(sizeof(TCelula));
-			ptr->direita->direita = pAux;
-			ptr->direita->Item = *pItem;
-			break;
-		}
-		x++;
-		ptr = ptr->direita; /*Proxima Celula
-		pAux = pAux->direita; /* próxima célula
-		//printf("-Aux: %d-\n", pAux->Item.Chave);
-		//printf("-PTR: %d-\n", ptr->Item.Chave);
-	}
-
-	return 1;
-}
-
-int LRetira(TLista* pLista, TItem* pItem)
-{
-	Apontador pAux;
-	if (LEhVazia(pLista))
-		return 0;
-	*pItem = pLista->pPrimeiro->direita->Item;
-	pAux = pLista->pPrimeiro;
-	pLista->pPrimeiro = pLista->pPrimeiro->direita;
-	free(pAux);
-	return 1;
-}
-
-int LRetiraPos(TLista* pLista, TItem* pItem, int pos)
-{
-	Apontador pAux;
-	Apontador ptr;
-	if (LEhVazia(pLista))
-		return 0;
-	int x = 0;
-	pAux = pLista->pPrimeiro->direita; // Cabeça -> Direita
-	ptr = pLista->pPrimeiro; //Começa na Cabeça
-	while (x <= pos)
-	{
-
-		if (x == pos)
-		{
-			//*pItem = pLista->pPrimeiro->direita->Item;// < Não alimente o PItem
-			ptr->direita = pAux->direita;
-			free(pAux);
-			break;
-		}
-		x++;
-		ptr = ptr->direita; /*Proxima Celula
-		pAux = pAux->direita; //próxima célula 
-		//printf("-Aux: %d-\n", pAux->Item.Chave);
-		//printf("-PTR: %d-\n", ptr->Item.Chave);
-	}
-	return 1;
-}
-*/
