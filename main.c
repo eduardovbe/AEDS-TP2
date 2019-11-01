2533cce64aa19b04823577a7e9648975d933b3f8
eduardovbe <51214452+eduardovbe@users.noreply.github.com>
2019-11-01 11:38:24 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int i,n;
+  FILE *lista;
+  lista = fopen("lista.txt","w");
+
+  scanf("%d",&n);
+  fprintf(lista,"%d",n);
+  fprintf(lista,"%s","\n");
+  for (i = 0; i < n*2; i++)
+  {
+    if((i % 2 == 0) && i != 0){
+        fprintf(lista,"%s","\n");
+    }
+    /* gerando valores aleatórios entre zero e 100 */
+    fprintf(lista,"%d ", rand() % 9);
+
+  }
+  fclose(lista);
+  return 0;
+}
+
